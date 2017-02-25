2ffb44e68114d94c0e0a8e0ca7dff13598759986
Oleksandr Diudiun <a.diudiun@gmail.com>
2017-02-25 18:20:17 +0200

    update examples and conditions

--- examples/route.php
+++ examples/route.php
@@ -1,26 +1,34 @@
 <?php
 declare (strict_types = 1);
-require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
 use memCrab\Exceptions\FileException;
 use memCrab\Exceptions\RoutingException;
+use memCrab\File\Yaml;
 use memCrab\Router\Router;
 
 try {
-	# Initialize Router
+
+	# Read routes from yaml
 	$Yaml = new Yaml();
+
 	$routes = $Yaml->load("../src/routs.example.yaml", null)->getContent();
 
+	# For enable cache You can use FileCache object as second parametr of
+	# $Yaml->load() function. Use memCrab\Cache library for it.
+	# Redis Cache: $FileCache = new RedisCache([Redis obj]);
+	# PHP file Cache: $FileCache = new PHPCache([PathToTMLFolder]);
+
+	# Initialize Router
 	$Router = new Router();
 	$Router->loadRoutes($routes);
 
 	# Routing
 	$Router->matchRoute("http://example.com/post/", "POST");
-	exit();
 	# Run your Controller|Service|Component
 	$ServiceName = $Router->getService();
+	$ActionName = $Router->getAction();
 	$Service = new $ServiceName();
-	$Action = $Router->getAction();
 	$Response = $Service->$Action($Router->getParams());
 } catch (RoutingException $error) {
 	$Respose = new \Response();
