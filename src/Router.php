6f325f490289bbd2e4ef241a2776707136e61778
alexshcherba <shcherbaalex77@gmail.com>
2020-06-26 19:24:01 +0300

    add fixes of params problems

--- src/Router.php
+++ src/Router.php
@@ -8,87 +8,93 @@ use memCrab\Exceptions\RoutingException;
  *  @author Oleksandr Diudiun
  */
 class Router {
-	private $routes;
-	private $params;
-	private $actionName;
-	private $serviceName;
-	private $errorMessage;
-	private $errorServiceName;
+    private $routes;
+    private $params;
+    private $actionName;
+    private $serviceName;
+    private $errorMessage;
+    private $errorServiceName;
 
-	function __construct() {}
+    function __construct(array $data = []) {
+        $this->serviceName = $data['serviceName'] ?? null;
+        $this->actionName = $data['actionName'] ?? null;
+        $this->params = $data['params'] ?? null;
+        $this->routes = [];
+    }
 
-	public function loadRoutes(array $routes) {
-		if (empty($routes)) {
-			throw new RoutingException(_("Empty routes"), 1);
-		}
+    public function loadRoutes(array $routes) {
+        if (empty($routes)) {
+            throw new RoutingException(_("Empty routes"), 1);
+        }
 
-		$this->routes = $routes;
-	}
+        $this->routes = $routes;
+    }
 
-	public function matchRoute(string $rawUrl, string $method): array{
-		$url = parse_url($rawUrl);
-		if (!isset($url['path']) || is_string($url['path']) === false) {
-			throw new RoutingException(_("Router can't parse request."), 400);
-		}
+    public function disposeData(string $rawUrl, string $method) {
+        $url = parse_url($rawUrl);
+        $routeData = [];
 
-		if (!is_array($this->routes)) {
-			throw new RoutingException(_("Can't find any routes rules."), 501);
-		}
-		$params = [];
-		foreach ($this->routes as $regExpString => $route) {
-			$routes = 0;
-			$result = preg_match("/^" . str_replace("/", "\/", $regExpString) . "$/u", $url['path'], $matches);
-			if ($result === 0) {
-				continue;
-			} elseif ($result === false) {
-				throw new RoutingException(_("Can't parse route RegExp. ") . $regExpString, 501);
-			} elseif ($result === 1) {
-				if (isset($route[$method])) {
-					$this->serviceName = $route[$method][0];
-					$this->actionName = $route[$method][1];
+        if (!isset($url['path']) || is_string($url['path']) === false) {
+            throw new RoutingException(_("Router can't parse request."), 400);
+        }
 
-					$paramsCount = count($route[$method]) - 2;
-					if ($paramsCount > 0) {
-						for ($i = 0; $i < $paramsCount; $i++) {
-							$params[$route[$method][$i + 2]] = $matches[$i + 1];
-						}
-					} else {
-						$params = array();
-					}
+        if (!is_array($this->routes)) {
+            throw new RoutingException(_("Can't find any routes rules."), 501);
+        }
 
-					$routes++;
-					break;
-				}
-			}
-		}
+        $routesCount = 0;
 
-		if ($routes === 0) {
-			throw new RoutingException(_("Not found route: ") . $method . " " . $rawUrl, 404);
-		}
+        foreach ($this->routes as $regExpString => $route) {
+            $result = preg_match("/^" . str_replace("/", "\/", $regExpString) . "$/u", $url['path'], $matches);
+            if ($result === 0) {
+                continue;
+            } elseif ($result === false) {
+                throw new RoutingException(_("Can't parse route RegExp. ") . $regExpString, 501);
+            } elseif ($result === 1 && isset($route[$method])) {
+                $routeData['serviceName'] = $route[$method][0];
+                $routeData['actionName'] = $route[$method][1];
 
-		if ($routes > 1) {
-			throw new RoutingException(_("Conflict. Multiple routes found."), 501);
-		}
-		return $params;
-	}
+                $paramsCount = count($route[$method]) - 2;
+                if ($paramsCount > 0) {
+                    for ($i = 0; $i < $paramsCount; $i++) {
+                        $routeData['params'][$route[$method][$i + 2]] = $matches[$i + 1];
+                    }
+                } else {
+                    $routeData['params'] = array();
+                }
 
-	public function getParams():  ? array{
-		return $this->params;
-	}
+                $routesCount++;
+                break;
+            }
+        }
 
-	public function getService() : string {
-		return $this->serviceName;
-	}
+        if ($routesCount > 1) {
+            throw new RoutingException(_("Conflict. Multiple routes found."), 501);
+        }
+        if ($routesCount === 0) {
+            throw new RoutingException(_("Route not found."), 501);
+        }
 
-	public function getAction():  ? string {
-		return $this->actionName;
-	}
+        return new Router($routeData);
+    }
 
-	public function getErrorMessage() :  ? string {
-		return $this->errorMessage;
-	}
+    public function getParams():  ? array{
+        return $this->params;
+    }
 
-	public function getErrorServiceName() : string {
-		return $this->errorServiceName;
-	}
+    public function getService() : string {
+        return $this->serviceName;
+    }
+
+    public function getAction():  ? string {
+        return $this->actionName;
+    }
+
+    public function getErrorServiceName() : string {
+        return $this->errorServiceName;
+    }
+
+    public function getErrorMessage():  ? string {
+        return $this->errorMessage;
+    }
 }
